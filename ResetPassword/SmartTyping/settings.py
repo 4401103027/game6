c476175efc09a435c7e32dde0311335b997df4e7
4401103027 <thanhngan1482000@gmail.com>
2022-01-13 11:19:47 +0700

    finish

--- ResetPassword/SmartTyping/settings.py
+++ ResetPassword/SmartTyping/settings.py
@@ -167,11 +167,11 @@ TAGGIT_CASE_INSENSITIVE = True
 
 #SMTP Configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tra112@nng1gmail.com'
-EMAIL_HOST_PASSWORD = 'nguyenbaotran'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'your-email'
+# EMAIL_HOST_PASSWORD = 'your-email-password'
 # EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 # EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
